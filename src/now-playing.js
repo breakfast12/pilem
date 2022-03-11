407aa6916795165160ebc4785642a1541c954327
Freza Ridho Septyawan <50305375+breakfast12@users.noreply.github.com>
2022-03-11 22:50:22 +0700

    pilem

---
+++ src/now-playing.js
@@ -0,0 +1,6 @@
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style/style.css";
+import "./component/app-bar.js";
+import nowplayingAll from "./view/nowplayingAll.js";
+nowplayingAll();
