db9ec3ca3ee8691ead941c887b28e7d0dbc5cf17
Freza Ridho Septyawan <50305375+breakfast12@users.noreply.github.com>
2022-03-11 22:52:41 +0700

    pilem

---
+++ src/detail.js
@@ -0,0 +1,7 @@
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style/style.css";
+import "./component/app-bar.js";
+import "./component/detail-movie.js";
+import detailsMovie from "./view/details.js";
+detailsMovie();
